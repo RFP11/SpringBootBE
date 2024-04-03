48f23e46b7945b964dc0365f94a469a33fc164ed
Raff <lolscam104@gmail.com>
2024-04-03 09:36:08 +0700

    first init

---
+++ src/main/java/com/rfp11/mybookrecipe/MybookrecipeApplication.java
@@ -0,0 +1,13 @@
+package com.rfp11.mybookrecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybookrecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybookrecipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rfp11/mybookrecipe/MybookrecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rfp11.mybookrecipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybookrecipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
